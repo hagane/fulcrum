65e897f9ef081158e84b1e707d58b83746787d9d
hagane <deemson@gmail.com>
2009-08-29 00:12:19 +0600

    evan moar comments

--- include/Timer.h
+++ include/Timer.h
@@ -4,14 +4,34 @@
 
 namespace FGF
 {
+	/** \brief Timer.
+	 *
+	 * Does chronometrage between two points in time.
+	 */
 	class EXPORT Timer
 	{
 	public:
 		Timer(void);
 		~Timer(void);
+
+		/**
+		 * Starts a time count.
+		 */
 		void start();
+
+		/**
+		 * Stops a time count
+		 */
 		void stop();
+
+		/**
+		 * Returns time passed between start() and stop() calls in seconds.
+		 */
 		float getDelta();
+
+		/**
+		 * Returns current FPS.
+		 */
 		float getFPS();
 	private:
 		LARGE_INTEGER freq,t_start,t_stop;
