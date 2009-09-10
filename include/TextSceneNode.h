b1198fb4dfd1dcdc3958c45bf9dc66016d344615
hagane <deemson@gmail.com>
2009-09-10 23:41:52 +0600

    TextSceneNode

---
+++ include/TextSceneNode.h
@@ -0,0 +1,20 @@
+#include <string>
+#include "export.h"
+#include "OpenGL.h"
+#include "SceneNode.h"
+#include "Font.h"
+
+namespace FGF
+{
+	class EXPORT TextSceneNode: public SceneNode
+	{
+	public:
+		TextSceneNode(SceneNode* parent, Font* f);
+		virtual ~TextSceneNode();
+		virtual void setString(std::wstring s);
+		virtual void Render(int curPass);
+	private:
+		std::wstring str;
+		Font* font;
+	};
+}
