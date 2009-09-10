7ef89740719caf1294e79d41e106375413e318ec
hagane <deemson@gmail.com>
2009-09-10 22:39:51 +0600

    bitmap fonts support

---
+++ include/Font.h
@@ -0,0 +1,49 @@
+#include <fstream>
+#include <map>
+#include <set>
+#include <string>
+#include "export.h"
+#include "OpenGL.h"
+#include "Texture.h"
+
+namespace FGF
+{
+	/**
+	 * This class represents a font.
+	 * Format of font description file:
+	 * <symbol> <origin_x> <origin_y> <width> <height>
+	 */
+	class EXPORT Font
+	{
+	public:
+		/**
+		 * A constructor.
+		 * @param font_desc File to read font description from
+		 * @param font_tex File with font texture
+		 */
+		Font(const char* font_desc, const char* font_tex);
+		~Font();
+
+		/**
+		 * Renders a char.
+		 */
+		void renderChar(wchar_t ch);
+		
+		/**
+		 * Returns a width of given character,
+		 * or 0 if there is no such character in
+		 * a font description.
+		 */
+		int getCharWidth(wchar_t ch);
+
+	private:
+		std::map<wchar_t,int> origin_x;
+		std::map<wchar_t,int> origin_y;
+		std::map<wchar_t,int> width;
+		std::map<wchar_t,int> height;
+
+		std::set<wchar_t> chars;
+
+		Texture* tex;
+	};
+}
---
+++ src/Font.cpp
@@ -0,0 +1,53 @@
+#include "../include/Font.h"
+namespace FGF
+{
+	Font::Font(const char* font_desc, const char* font_tex)
+	{
+		tex = new Texture(font_tex);
+		std::wifstream f;
+		f.open(font_desc);
+		wchar_t ch;
+		int origin_x_val;
+		int origin_y_val;
+		int width_val;
+		int height_val;
+		while(!f.eof())
+		{
+			((((f >> ch) >> origin_x_val) >> origin_y_val) >> width_val) >> height_val;
+			origin_x[ch] = origin_x_val;
+			origin_y[ch] = origin_y_val;
+			width[ch] = width_val;
+			height[ch] = height_val;
+			chars.insert(ch);
+		}
+		f.close();
+	}
+
+	void Font::renderChar(wchar_t ch)
+	{
+		tex->Activate();
+		if(chars.find(ch) != chars.end())
+		{
+			glTexCoord2i(origin_x[ch], origin_y[ch]);
+			glVertex3i(0,0,0);
+
+			glTexCoord2i(origin_x[ch], origin_y[ch] + height[ch]);
+			glVertex3i(0,height[ch],0);
+
+			glTexCoord2i(origin_x[ch] + width[ch], origin_y[ch] + height[ch]);
+			glVertex3i(width[ch],height[ch],0);
+
+			glTexCoord2i(origin_x[ch] + width[ch], origin_y[ch]);
+			glVertex3i(width[ch],0,0);
+		}
+	}
+
+	int Font::getCharWidth(wchar_t ch)
+	{
+		if(chars.find(ch) != chars.end())
+		{
+			return width[ch];
+		}
+		return 0;
+	}
+}
